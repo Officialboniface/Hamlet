4306ee8093a64ce19bbe74ae0cb829dc3e49009e
Officialboniface <officialboniface@gmail.com>
2020-08-25 14:14:10 -0700

    Setup the manager, company, employee's personal and employment info profiles with their tablayout's and adapters respectively.

---
+++ app/src/main/java/com/example/hamlet/ui/ProfileActivity.kt
@@ -0,0 +1,36 @@
+package com.example.hamlet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hamlet.R
+import com.example.hamlet.adapters.EmployeeViewPagerAdapter
+import com.example.hamlet.adapters.ProfileViewPagerAdapter
+import com.example.hamlet.fragments.companyfragment.CompanyDetailsFragment
+import com.example.hamlet.fragments.companyfragment.ManagerDetailsFragment
+import com.example.hamlet.fragments.employeefragment.EmploymentInfoFragment
+import com.example.hamlet.fragments.employeefragment.PersonalInfoFragment
+import kotlinx.android.synthetic.main.activity_employees_details.*
+import kotlinx.android.synthetic.main.activity_profile.*
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+        setupTabs()
+    }
+
+
+
+    private fun setupTabs() {
+        val adapter = ProfileViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(ManagerDetailsFragment(), "Manager Profile")
+        adapter.addFragment(CompanyDetailsFragment(), "Company Details")
+
+        profile_viewPager.adapter = adapter
+        profile_tab.setupWithViewPager(profile_viewPager)
+
+    }
+
+}
