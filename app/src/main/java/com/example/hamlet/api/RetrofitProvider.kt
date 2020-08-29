7e8394d73a32fa58888f3aae5878e42023de5e8d
Officialboniface <officialboniface@gmail.com>
2020-08-29 16:02:45 -0700

    Retrofit  Updated

---
+++ app/src/main/java/com/example/hamlet/api/RetrofitProvider.kt
@@ -0,0 +1,16 @@
+package com.example.hamlet.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitProvider {
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(AppConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(ApiCalls::class.java)
+
+    }
+
+}
--- app/src/main/java/com/example/hamlet/model/User.kt
+++ app/src/main/java/com/example/hamlet/model/User.kt
@@ -3,6 +3,5 @@ package com.example.hamlet.model
 data class User(
     val id: Int,
     val token: String,
-    val email: String,
-    val username: String
+    val email: String
 )
